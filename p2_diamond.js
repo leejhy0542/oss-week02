1369d3f799d7254068d01dd0dc96b91bdaec9525
JuHyeong Lee <leejhy0542@naver.com>
2026-09-10 10:48:10 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -10,6 +10,35 @@
 //   *
 
 const n = Number(process.argv[2] ?? 5);
+let a = 1;
+let b = n/2-0.5;
+let str = "";
+for(let i = 0; i<n/2+0.5;i++){
+    for(let j = 0;j<b;j++){
+        str += " ";
+    }
+    for(let k = 0;k<a;k++){
+        str += "*";
+    }
+    console.log(str);
+    str = "";
+    b--;
+    a+=2;
+}
+b+=2;
+a-=4;
+for(let i = 0;i<n/2-0.5;i++){
+    for(let j = 0;j<b;j++){
+        str += " ";
+    }
+    for(let k = 0;k<a;k++){
+        str += "*";
+    }
+    console.log(str);
+    str = "";
+    b++;
+    a-=2;
+}
 
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
